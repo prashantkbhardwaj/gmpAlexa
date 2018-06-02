5bd6d069dc39aea00a53362457f49580e1e57c39
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2018-06-02 13:20:56 +0530

    fix

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+    $lat = "0";
+    $lon = "0";
+    $placeName = "0";
+    if (isset($_GET['lat'])) { $lat = $_GET['lat']; } else { $lat = "0"; }
+    if (isset($_GET['lon'])) { $lon = $_GET['lon']; } else { $lon = "0"; }
+    if (isset($_GET['placeName'])) { $placeName = $_GET['placeName'];   } else { $placeName = "0"; }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,44 +23,57 @@
         </tr>
     </table>
     <br><br>  
-    <textarea  id="responsecontainer" ></textarea>
-    <script>    
-        function initialize()
-        {        
-            var marker;     
-            var marker_info;
-            var mapProp = { 
-                center: new google.maps.LatLng(0,0),        
-                zoom:18,
-                mapTypeId: 'roadmap'
-            };  
-            var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-            
-                $("#responsecontainer").load("select.php");
-                //var refreshId = setInterval(function() {
-                    //$("#responsecontainer").load('select.php?randval='+ Math.random());
-                    var x = document.getElementById("responsecontainer").value;
-                    var locate = x.split(',');
-                    var position=new google.maps.LatLng(locate[0], locate[1]);       
-                    if(marker != null){
-                        marker.setMap(null);          
+    <textarea style="display:none;" id="responsecontainer" ><?php echo $lat.",".$lon.",".$placeName; ?></textarea>
+    <?php
+        if ($lat == "0" && $lon == "0" && $placeName == "0") { ?>
+            <style type="text/css">
+                body {
+                    background-color: lightblue;
+                }
+            </style>
+            <?php
+        }else { ?>
+                <script>    
+                    function initialize()
+                    {        
+                        var marker;     
+                        var marker_info;
+                        var mapProp = { 
+                            center: new google.maps.LatLng(0,0),        
+                            zoom:18,
+                            mapTypeId: 'roadmap'
+                        };  
+                        var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+                            //$("#responsecontainer").load("select.php");
+                            //var refreshId = setInterval(function() {
+                               // $("#responsecontainer").load('select.php?randval='+ Math.random());
+                                var x = document.getElementById("responsecontainer").value;
+                                var locate = x.split(',');
+                                var position=new google.maps.LatLng(locate[0], locate[1]);       
+                                if(marker != null){
+                                    marker.setMap(null);          
+                                }
+                                marker = new google.maps.Marker({
+                                    position: position,
+                                    map: map,            
+                                });
+                                map.setCenter(position);    
+                                marker_info = new google.maps.InfoWindow({
+                                  content: locate[2]
+                                }); 
+                                marker_info.open(map,marker);               
+                                                                      
+                            // }, 1000);
+                            // $.ajaxSetup({ cache: false});       
+                         
                     }
-                    marker = new google.maps.Marker({
-                        position: position,
-                        map: map,            
-                    });
-                    map.setCenter(position);    
-                    marker_info = new google.maps.InfoWindow({
-                      content: locate[2]
-                    }); 
-                    marker_info.open(map,marker);               
-                                                          
-                //}, 1000);
-                //$.ajaxSetup({ cache: false});       
-             
+                    google.maps.event.addDomListener(window, 'load', initialize);       
+                </script>
+
+            <?php
         }
-        google.maps.event.addDomListener(window, 'load', initialize);       
-    </script>  
+    ?>
+      
 
 </body>
 
