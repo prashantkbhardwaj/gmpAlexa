85fbaaee77df9035d58a34d01dde924cbab8eff4
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2018-06-02 17:35:36 +0530

    fix

--- place.php
+++ place.php
@@ -6,9 +6,7 @@
     if (isset($_GET['lat'])) { $lat = $_GET['lat']; } else { $lat = "0"; }
     if (isset($_GET['lon'])) { $lon = $_GET['lon']; } else { $lon = "0"; }
     if (isset($_GET['placeName'])) { $placeName = $_GET['placeName'];   } else { $placeName = "0"; }
-    if (isset($_GET['address'])) { $address = $_GET['address']; } else { $address = "0"; }
-    if (isset($_GET['eta'])) { $eta = $_GET['eta']; } else { $eta = "0"; }
 
-    $query = "INSERT INTO pings (lat, lon, placeName, address, eta) VALUES ('{$lat}', '{$lon}', '{$placeName}', '{$address}', '{$eta}')";
+    $query = "INSERT INTO pings (lat, lon, placeName) VALUES ('{$lat}', '{$lon}', '{$placeName}')";
    	mysqli_query($conn, $query);
 ?>
